6723bdcd0975ec4f775858bdac791a8212f03498
Carwyn Ellis <carwynellis@gmail.com>
2018-01-01 19:23:10 +0000

    Solutions for part iii exercises 28-31

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -8,10 +8,14 @@ data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int): Comparabl
         else                 -> dayOfMonth - other.dayOfMonth
     }
 
-
 }
 
 operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
+
+operator fun MyDate.plus(interval: TimeInterval) = this.addTimeIntervals(interval, 1)
+
+operator fun MyDate.plus(repeatedInterval: RepeatedTimeInterval) =
+    this.addTimeIntervals(repeatedInterval.interval, repeatedInterval.occurences)
 
 enum class TimeInterval {
     DAY,
@@ -19,10 +23,31 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate) {
+operator fun TimeInterval.times(n: Int) = RepeatedTimeInterval(this, n)
+
+
+class DateRange(val start: MyDate, val endInclusive: MyDate): Iterable<MyDate> {
 
     // Note - converting this to a range check as suggested by intellij will result
     // in a stack overflow presumably due to the rangeTo extenstion function above.
     operator fun contains(d: MyDate): Boolean = d >= start && d <= endInclusive
 
+    override fun iterator(): Iterator<MyDate> = DateIterator(DateRange(start, endInclusive))
+
 }
+
+class DateIterator(val dateRange: DateRange): Iterator<MyDate> {
+
+    private var current = dateRange.start
+
+    override fun hasNext(): Boolean = current <= dateRange.endInclusive
+
+    override fun next(): MyDate {
+        val nextDate = current
+        current = current.nextDay()
+        return nextDate
+    }
+
+}
+
+class RepeatedTimeInterval(val interval: TimeInterval, val occurences: Int)
--- src/iii_conventions/n31Invoke.kt
+++ src/iii_conventions/n31Invoke.kt
@@ -3,7 +3,31 @@ package iii_conventions
 import util.TODO
 
 
-class Invokable
+/**
+ * Solution from the answers. This initially made no sense to me whatsoever.
+ *
+ * Turns out invoke can be called on any instance of a class without a method name!
+ *
+ * So if val foo = Invokable(0) you can call invoke using foo() rather than foo.invoke().
+ *
+ * It appears that successive invocations can be made my specifying multiple ()s e.g.
+ *  foo()()() // calls invoke 3 times
+ *
+ * This looks like Scala's apply - but the multiple invocations are surprising to me at least.
+ *
+ * More info here
+ *   - http://joshskeen.com/kotlins-invoke-operator/
+ *   - https://www.51zero.com/blog/2016/4/14/kotlin-for-scala-developers
+ */
+class Invokable(private var invocations: Int = 0) {
+
+    operator fun invoke(): Invokable {
+        invocations++
+        return this
+    }
+
+    fun getNumberOfInvocations() = invocations
+}
 
 fun todoTask31(): Nothing = TODO(
     """
@@ -14,6 +38,5 @@ fun todoTask31(): Nothing = TODO(
     references = { invokable: Invokable -> })
 
 fun task31(invokable: Invokable): Int {
-    todoTask31()
-//    return invokable()()()().getNumberOfInvocations()
+    return invokable()()()().getNumberOfInvocations()
 }
